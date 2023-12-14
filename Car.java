ae6a4b14a5f34825c29f860cac9e8276412956e1
myimyaem <137817981+myimyaem@users.noreply.github.com>
2023-12-14 09:33:04 +0000

    Day 8 java basics

---
+++ Car.java
@@ -0,0 +1,36 @@
+/**
+ * Car object
+ * asdd
+ * asdasd
+ * asdasdd 
+ * asdasd
+ * asdasd
+ */
+public class Car {
+    // this is a car properties  (Single Line comment )
+    String name;
+    int NumberOfDoors;
+    /*
+     * this
+     * is 
+     * a 
+     * multiline comment
+     */
+
+     // this method get the number of door from current car object 
+
+     /**
+         * asd
+         * asd
+         * asd
+         * as
+         * asd
+         * asd
+         * asdad34
+         * aasdasd
+         */
+    void getNumberOfDoors(){
+        
+    }
+    class D{}
+}
--- Main.java
+++ Main.java
@@ -1,3 +1,22 @@
+import java.util.Random;
+
 public class Main{
   
+  public static void main(String[] args) {
+    Car vois = new Car();
+    vois.getNumberOfDoors();
+    Random r = new Random();
+    System.out.println(r.nextInt(20));
+    A joy = new A();
+  }
+
 }
+  class A{
+
+  }
+  class B{
+    
+  }
+  class C{
+
+  }
